dd9f60dc8e2e9fee33d1ecec2489a68d371f308a
Simple4H <crescentcxm@gmail.com>
2019-02-25 16:18:22 +0800

    swagger2

---
+++ src/main/java/com/simple/pojo/User.java
@@ -0,0 +1,34 @@
+package com.simple.pojo;
+
+/**
+ * Create By S I M P L E On 2019/02/25 16:06:19
+ */
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
