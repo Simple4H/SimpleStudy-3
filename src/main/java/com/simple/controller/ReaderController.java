76b08fae8c5bff3102362e97c5c781c87e11ffde
Simple4H <crescentcxm@gmail.com>
2019-04-10 11:15:49 +0800

    excel导出

---
+++ src/main/java/com/simple/controller/ReaderController.java
@@ -0,0 +1,109 @@
+package com.simple.controller;
+
+import com.simple.entity.Reader;
+import com.simple.mapper.ReaderRepository;
+import com.simple.util.ExcelUtil;
+import org.apache.http.HttpResponse;
+import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Create By Simple4H
+ * Date: 2019-04-08 14:51
+ */
+@RestController
+@RequestMapping(value = "/excel/")
+public class ReaderController {
+
+    @Autowired
+    private ReaderRepository readerRepository;
+
+    @RequestMapping(value = "download")
+    public Object getExcel() throws IOException {
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletResponse response = servletRequestAttributes.getResponse();
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet("统计表");
+        createTitle(workbook, sheet);
+
+        List<Reader> lists = (List<Reader>) readerRepository.findAll();
+
+        //设置日期格式
+        HSSFCellStyle style = workbook.createCellStyle();
+        style.setDataFormat(HSSFDataFormat.getBuiltinFormat("m/d/yy h:mm"));
+
+        //新增数据行，并且设置单元格数据
+        int rowNum = 1;
+        for (Reader reader : lists) {
+
+            HSSFRow row = sheet.createRow(rowNum);
+            row.createCell(0).setCellValue(reader.getId());
+            row.createCell(1).setCellValue(reader.getUsername());
+            row.createCell(2).setCellValue(reader.getPassword());
+            row.createCell(3).setCellValue(reader.getAge());
+            row.createCell(4).setCellValue(reader.getSex());
+            HSSFCell cell = row.createCell(5);
+            cell.setCellValue(new Date());
+            cell.setCellStyle(style);
+            rowNum++;
+        }
+
+        //生成excel文件
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-disposition", "attachment;filename=student.xlsx");
+
+        //刷新缓冲
+        response.flushBuffer();
+
+        //workbook将Excel写入到response的输出流中，供页面下载
+        workbook.write(response.getOutputStream());
+        return "download.xlsx";
+    }
+
+    private void createTitle(HSSFWorkbook workbook, HSSFSheet sheet) {
+        HSSFRow row = sheet.createRow(0);
+        //设置列宽，setColumnWidth的第二个参数要乘以256，这个参数的单位是1/256个字符宽度
+        sheet.setColumnWidth(2, 12 * 256);
+        sheet.setColumnWidth(3, 17 * 256);
+
+        //设置为居中加粗
+        HSSFCellStyle style = workbook.createCellStyle();
+        HSSFFont font = workbook.createFont();
+        font.setBold(true);
+        style.setAlignment(HorizontalAlignment.CENTER);
+        style.setFont(font);
+
+        HSSFCell cell;
+        cell = row.createCell(0);
+        cell.setCellValue("序号");
+        cell.setCellStyle(style);
+
+        cell = row.createCell(1);
+        cell.setCellValue("姓名");
+        cell.setCellStyle(style);
+
+        cell = row.createCell(2);
+        cell.setCellValue("密码");
+        cell.setCellStyle(style);
+
+        cell = row.createCell(3);
+        cell.setCellValue("年龄");
+        cell.setCellStyle(style);
+
+        cell = row.createCell(4);
+        cell.setCellValue("性别");
+        cell.setCellStyle(style);
+    }
+}
---
+++ src/main/java/com/simple/mapper/ReaderRepository.java
@@ -0,0 +1,11 @@
+package com.simple.mapper;
+
+import com.simple.entity.Reader;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Create By Simple4H
+ * Date: 2019-04-08 14:51
+ */
+public interface ReaderRepository extends CrudRepository<Reader, Integer> {
+}
